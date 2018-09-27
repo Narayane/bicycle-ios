97ab83938badc41b243c07e4ba0d9cce67ab85ff
Sébastien BALARD <sebastien.balard@gmail.com>
2018-09-28 00:08:32 +0200

    add settings bundle, bind with user defaults

--- Bicycle/Bicycle/Views/BICDataPermissionsViewController.swift
+++ Bicycle/Bicycle/Views/BICDataPermissionsViewController.swift
@@ -25,8 +25,10 @@ class BICDataPermissionsViewController: UIViewController {
     @IBOutlet weak var labelDataWarning: UILabel!
     @IBOutlet weak var labelCrashDataText: UILabel!
     @IBOutlet weak var labelCrashDataSwitch: UILabel!
+    @IBOutlet weak var switchAllowCrashDataSending: UISwitch!
     @IBOutlet weak var labelUseDataText: UILabel!
     @IBOutlet weak var labelUseDataSwitch: UILabel!
+    @IBOutlet weak var switchAllowUseDataSending: UISwitch!
     @IBOutlet weak var labelDataPermissionsWarning: UILabel!
     @IBOutlet weak var buttonValidate: UIButton!
     
@@ -35,6 +37,8 @@ class BICDataPermissionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initLayout()
+        observeEvents()
+        viewModel?.loadDataSendingPermissions()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,8 +46,14 @@ class BICDataPermissionsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: Fileprivate methods
     
+    // MARK: UI events
+    @IBAction func buttonValidatedidTouch(_ sender: UIButton) {
+        log.i("set initial data sending permissions: crash (\(switchAllowCrashDataSending.isOn)), use (\(switchAllowUseDataSending.isOn))")
+        viewModel?.saveDataSendingPermissions(allowCrashDataSending: switchAllowCrashDataSending.isOn, allowUseDataSending: switchAllowUseDataSending.isOn)
+    }
+    
+    // MARK: Fileprivate methods
     fileprivate func initLayout() {
         labelTitle.text = "bic_onboarding_screen_title".localized()
         labelDataWarning.text = "bic_onboarding_screen_personal_data".localized()
@@ -54,7 +64,31 @@ class BICDataPermissionsViewController: UIViewController {
         labelDataPermissionsWarning.text = "bic_onboarding_screen_warning".localized()
         buttonValidate.setTitle("bic_actions_validate_choices".localized(), for: .normal)
     }
-
+    
+    fileprivate func observeEvents() {
+        launch {
+            viewModel?.events.asObservable().observeOn(MainScheduler.instance).subscribe({ (rx) in
+                guard let event = rx.element else { return }
+                log.v("event -> \(String(describing: type(of: event)))")
+                switch (event) {
+                case is EventDataSendingPermissionsLoaded:
+                    guard let event = event as? EventDataSendingPermissionsLoaded else { return }
+                    self.switchAllowUseDataSending.setOn(event.allowCrashDataSending, animated: false)
+                    self.switchAllowUseDataSending.setOn(event.allowUseDataSending, animated: false)
+                case is EventDataSendingPermissionsSet:
+                    DispatchQueue.main.async {
+                        let viewController = BICHomeViewController()
+                        let navigationController = UINavigationController(rootViewController: viewController)
+                        let primaryColor = UIColor(hex: (Bundle.main.object(forInfoDictionaryKey: "BICPrimaryColor") as? String)!)
+                        navigationController.styleNavigationBar(barTintColor: primaryColor, tintColor: UIColor.white)
+                        navigationController.modalTransitionStyle = .crossDissolve
+                        self.present(navigationController, animated: true, completion: nil)
+                    }
+                default: break
+                }
+            })
+        }
+    }
 }
 
 //MARK: - StoryboardInstantiatable
